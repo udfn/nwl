a47d3d23f1041a3134383d29bd16419a1a9b07c0
Undefined Name <74594673+udfn@users.noreply.github.com>
2023-04-03 22:02:55 +0300

    Zig bindings update
    Abuse namespacing! There's still more to be done here, though. Shuffled
    some things around, renamed things, fixed things, un-pub'd things.

--- zig/nwl.zig
+++ zig/nwl.zig
@@ -5,14 +5,14 @@
 
 const std = @import("std");
 
-const GlobalIml = extern struct {
-    destroy: ?*const fn (?*anyopaque) callconv(.C) void,
-};
 pub const Global = extern struct {
+    const Impl = extern struct {
+        destroy: ?*const fn (?*anyopaque) callconv(.C) void,
+    };
     link: WlList,
     name: u32,
     global: ?*anyopaque,
-    impl: GlobalIml,
+    impl: Impl,
 };
 
 /// CamelCasify a Wayland object name, to match zig-wayland
@@ -97,12 +97,6 @@ pub const ZwlrLayerSurfaceV1 = WaylandObject("zwlr_layer_surface_v1");
 pub const XkbContext = opaque {};
 pub const WlCursorTheme = opaque {};
 pub const Poll = opaque {};
-const StateEventsImpl = extern struct {
-    output_new: ?*const fn (*Output) callconv(.C) void = null,
-    output_destroy: ?*const fn (*Output) callconv(.C) void = null,
-    global_add: ?*const fn (*State, *const WlRegistry, u32, [*:0]const u8, u32) callconv(.C) bool = null,
-    global_remove: ?*const fn (*State, *const WlRegistry, u32) callconv(.C) void = null,
-};
 
 pub const Output = extern struct {
     state: *State,
@@ -165,24 +159,17 @@ pub fn WlListHead(comptime linktype:type, comptime field: std.meta.FieldEnum(lin
     };
 }
 
-pub const nwl_surface_generic_func_t = *const fn (*Surface) callconv(.C) void;
-pub const RendererImpl = extern struct {
-    apply_size: nwl_surface_generic_func_t,
-    surface_destroy: nwl_surface_generic_func_t,
-    swap_buffers: *const fn (*Surface, i32, i32) callconv(.C) void,
-    render: nwl_surface_generic_func_t,
-    destroy: nwl_surface_generic_func_t,
-};
-pub const Renderer = extern struct {
-    impl: ?*const RendererImpl,
-    data: ?*anyopaque,
-    rendering: bool,
-};
-pub const WlArray = extern struct {
-    size: usize,
-    alloc: usize,
-    data: ?*anyopaque,
-};
+fn WlArray(comptime T:type) type {
+    return extern struct {
+        size: usize,
+        alloc: usize,
+        data: ?[*]T,
+
+        pub fn slice(self:@This()) []T {
+            return self.data.?[0..self.size/@sizeOf(T)];
+        }
+    };
+}
 
 pub const WlFixed = enum(i32) {
     _,
@@ -206,85 +193,6 @@ pub const WlFixed = enum(i32) {
     }
 };
 
-pub const xkb_keysym_t = u32;
-pub const xkb_keycode_t = u32;
-
-const SurfaceFlags = packed struct(u32) {
-    no_autoscale: bool,
-    no_autocursor: bool,
-    nwl_frees: bool,
-    padding: u29
-};
-
-const SurfaceState = packed struct(u32) {
-    active: bool,
-    maximized: bool,
-    fullscreen: bool,
-    resizing: bool,
-    tiled_left: bool,
-    tiled_right: bool,
-    tiled_top: bool,
-    tiled_bottom: bool,
-    csd: bool,
-    needs_draw: bool,
-    needs_applysize: bool,
-    destroy: bool,
-    needs_configure: bool,
-    _padding:u19
-};
-
-const XdgWmCaps = packed struct(u8) {
-    window_menu: bool,
-    maximize: bool,
-    fullscreen: bool,
-    minimize: bool,
-    _pad:u4
-};
-
-const SurfaceRoleUnion = extern union {
-    toplevel: extern struct {
-        wl: *XdgToplevel,
-        decoration: ?*ZxdgToplevelDecorationV1,
-        wm_capabilities:XdgWmCaps,
-        bounds_width:i32,
-        bounds_height:i32
-    },
-    popup: extern struct {
-        wl: *XdgPopup,
-        lx:i32,
-        ly:i32,
-        reposition_token:u32
-    },
-    layer: extern struct {
-        wl: *ZwlrLayerSurfaceV1
-    },
-    subsurface: extern struct {
-        wl: *WlSubsurface
-    },
-};
-
-pub const nwl_surface_destroy_t = ?*const fn (*Surface) callconv(.C) void;
-pub const nwl_surface_input_pointer_t = ?*const fn (*Surface, *Seat, *PointerEvent) callconv(.C) void;
-pub const nwl_surface_input_keyboard_t = ?*const fn (*Surface, *Seat, *KeyboardEvent) callconv(.C) void;
-pub const nwl_surface_configure_t = ?*const fn (*Surface, u32, u32) callconv(.C) void;
-pub const nwl_surface_dnd_t = ?*const fn(*Surface, *Seat, *DndEvent) callconv(.C) void;
-
-const SurfaceImpl = extern struct {
-    destroy: nwl_surface_destroy_t,
-    input_pointer: nwl_surface_input_pointer_t,
-    input_keyboard: nwl_surface_input_keyboard_t,
-    dnd: nwl_surface_dnd_t,
-    configure: nwl_surface_configure_t,
-    close: ?*const fn (*Surface) callconv(.C) void,
-};
-
-const SurfaceRoleId = enum(u8) { none, toplevel, popup, layer, sub, cursor, dragicon };
-
-const SurfaceOutputs = extern struct {
-    outputs: [*]*Output,
-    amount: u32,
-};
-
 const DataDevice = extern struct {
     wl: ?*WlDataDevice,
     drop: DataOffer,
@@ -292,56 +200,59 @@ const DataDevice = extern struct {
     incoming: DataOffer,
     event: DndEvent,
 };
-pub const struct_xkb_keymap = opaque {};
-pub const struct_xkb_state = opaque {};
-pub const struct_xkb_compose_state = opaque {};
-pub const struct_xkb_compose_table = opaque {};
 pub const KeyboardEvent = extern struct {
-    type: KeyboardEventType,
-    compose_state: KeyboardComposeState,
+    const EventType = enum(u8) {
+        focus,
+        keydown,
+        keyup,
+        keyrepeat,
+        modifiers,
+    };
+    const XkbKeysym = u32;
+    const XkbKeycode = u32;
+    const ComposeState = enum(u8) { none, composing, composed };
+    type: EventType,
+    compose_state: ComposeState,
     focus: bool,
-    keysym: xkb_keysym_t,
-    keycode: xkb_keycode_t,
+    keysym: XkbKeysym,
+    keycode: XkbKeycode,
     utf8: [16]u8,
     serial: u32,
 };
-pub const WlCursor = opaque {};
-
-const PointerEventChanged = packed struct(u8) { focus: bool, button: bool, motion: bool, axis: bool, _padding: u4 };
-
-const PointerButtons = packed struct(u8) { left: bool, middle: bool, right: bool, back: bool, forward: bool, _padding: u3 };
-
-const PointerAxisSource = packed struct(u8) {
-    wheel:bool,
-    finger:bool,
-    continuous:bool,
-    wheel_tilt:bool,
-    _pad:u4
-};
-
-const PointerAxis = packed struct(u8) {
-    vertical:bool,
-    horizontal:bool,
-    _pad:u6
-};
 
 pub const PointerEvent = extern struct {
-    changed: PointerEventChanged,
+    const Changed = packed struct(u8) { focus: bool, button: bool, motion: bool, axis: bool, _padding: u4 };
+    const Buttons = packed struct(u8) { left: bool, middle: bool, right: bool, back: bool, forward: bool, _padding: u3 };
+    const AxisSource = packed struct(u8) {
+        wheel:bool,
+        finger:bool,
+        continuous:bool,
+        wheel_tilt:bool,
+        _pad:u4
+    };
+    const Axis = packed struct(u8) {
+        vertical:bool,
+        horizontal:bool,
+        _pad:u6
+    };
+    changed: Changed,
     serial: u32,
     surface_x: WlFixed,
     surface_y: WlFixed,
-    buttons: PointerButtons,
-    buttons_prev: PointerButtons,
+    buttons: Buttons,
+    buttons_prev: Buttons,
     axis_value_vert: i32,
     axis_value_hori: i32,
     axis_hori: WlFixed,
     axis_vert: WlFixed,
-    axis_source: PointerAxisSource,
-    axis_stop: PointerAxis,
+    axis_source: AxisSource,
+    axis_stop: Axis,
     focus: bool,
 };
 
+
 const PointerSurface = extern struct {
+    const WlCursor = opaque {};
     xcursor: ?*WlCursor,
     xcursor_surface: ?*WlSurface,
     nwl: ?*Surface,
@@ -350,18 +261,23 @@ const PointerSurface = extern struct {
 };
 
 pub const Seat = extern struct {
+    const XkbKeymap = opaque {};
+    const XkbState = opaque {};
+    const XkbComposeState = opaque {};
+    const XkbComposeTable = opaque {};
+
     state: *State,
     link: WlList,
     wl_seat: *WlSeat,
     data_device: DataDevice,
     keyboard: ?*WlKeyboard,
-    keyboard_keymap: ?*struct_xkb_keymap,
-    keyboard_state: ?*struct_xkb_state,
+    keyboard_keymap: ?*XkbKeymap,
+    keyboard_state: ?*XkbState,
     keyboard_context: ?*XkbContext,
     keyboard_compose_enabled: bool,
     keyboard_repeat_enabled: bool,
-    keyboard_compose_state: ?*struct_xkb_compose_state,
-    keyboard_compose_table: ?*struct_xkb_compose_table,
+    keyboard_compose_state: ?*XkbComposeState,
+    keyboard_compose_table: ?*XkbComposeTable,
     keyboard_focus: ?*Surface,
     keyboard_repeat_rate: i32,
     keyboard_repeat_delay: i32,
@@ -378,15 +294,14 @@ pub const Seat = extern struct {
     name: [*:0]u8,
 };
 
-pub const DndEventType = enum(u8) {
-    motion = 0,
-    enter,
-    left,
-    drop
-};
-
 pub const DndEvent = extern struct {
-    type: DndEventType,
+    const EventType = enum(u8) {
+        motion = 0,
+        enter,
+        left,
+        drop
+    };
+    type: EventType,
     serial: u32,
     focus_surface: ?*Surface,
     x: WlFixed,
@@ -397,23 +312,12 @@ pub const DndEvent = extern struct {
 };
 
 pub const DataOffer = extern struct {
-    mime: WlArray,
+    mime: WlArray([*:0]u8),
     offer: ?*WlDataOffer,
 };
 
-const KeyboardEventType = enum(u8) {
-    focus,
-    keydown,
-    keyup,
-    keyrepeat,
-    modifiers,
-};
-const KeyboardComposeState = enum(u8) { none, composing, composed };
-
-pub const nwl_poll_callback_t = *const fn (*State, ?*anyopaque) callconv(.C) void;
-
 pub const cairo_surface_t = opaque {};
-pub const nwl_surface_cairo_render_t = *const fn (*Surface, *cairo_surface_t) callconv(.C) void;
+const nwl_surface_cairo_render_t = *const fn (*Surface, *cairo_surface_t) callconv(.C) void;
 extern fn nwl_surface_renderer_cairo(surface: *Surface, renderfunc: nwl_surface_cairo_render_t, flags: i32) void;
 pub const surfaceRendererCairo = nwl_surface_renderer_cairo;
 
@@ -421,38 +325,115 @@ extern fn wl_proxy_marshal(p: ?*WlProxy, opcode: u32, ...) void;
 
 const Error = error{ InitFailed, RoleSetFailed, SurfaceCreateFailed };
 
-const SurfaceEvents = struct { destroy: ?*const fn (*const Surface) void = null };
-
 pub const Surface = extern struct {
-    link: WlList,
-    dirtlink: WlList,
-    state: *State,
+    const GenericSurfaceFn = *const fn (*Surface) callconv(.C) void;
+    const RoleId = enum(u8) { none, toplevel, popup, layer, sub, cursor, dragicon };
+    const Flags = packed struct(u32) {
+        no_autoscale: bool = false,
+        no_autocursor: bool = false,
+        nwl_frees: bool = false,
+        padding: u29 = 0
+    };
+
+    const SurfaceStates = packed struct(u32) {
+        active: bool,
+        maximized: bool,
+        fullscreen: bool,
+        resizing: bool,
+        tiled_left: bool,
+        tiled_right: bool,
+        tiled_top: bool,
+        tiled_bottom: bool,
+        csd: bool,
+        needs_draw: bool,
+        needs_applysize: bool,
+        destroy: bool,
+        needs_configure: bool,
+        _padding:u19
+    };
+
+    pub const Renderer = extern struct {
+        pub const Impl = extern struct {
+            // Bug or feature? Using GenericSurfaceFn here is a dependency loop error..
+            apply_size: *const fn (*Surface) callconv(.C) void,
+            surface_destroy: *const fn (*Surface) callconv(.C) void,
+            swap_buffers: *const fn (*Surface, i32, i32) callconv(.C) void,
+            render: *const fn (*Surface) callconv(.C) void,
+            destroy: *const fn (*Surface) callconv(.C) void,
+        };
+        impl: ?*const Impl = null,
+        data: ?*anyopaque = undefined,
+        rendering: bool = false,
+    };
+    const SurfaceImpl = extern struct {
+        destroy: ?GenericSurfaceFn = null,
+        input_pointer: ?*const fn (*Surface, *Seat, *PointerEvent) callconv(.C) void = null,
+        input_keyboard: ?*const fn (*Surface, *Seat, *KeyboardEvent) callconv(.C) void = null,
+        dnd: ?*const fn(*Surface, *Seat, *DndEvent) callconv(.C) void = null,
+        configure: ?*const fn (*Surface, u32, u32) callconv(.C) void = null,
+        close: ?GenericSurfaceFn = null,
+    };
+    const RoleUnion = extern union {
+        toplevel: extern struct {
+            const Capabilities = packed struct(u8) {
+                window_menu: bool,
+                maximize: bool,
+                fullscreen: bool,
+                minimize: bool,
+                _pad:u4
+            };
+            wl: *XdgToplevel,
+            decoration: ?*ZxdgToplevelDecorationV1,
+            wm_capabilities:Capabilities,
+            bounds_width:i32,
+            bounds_height:i32
+        },
+        popup: extern struct {
+            wl: *XdgPopup,
+            lx:i32,
+            ly:i32,
+            reposition_token:u32
+        },
+        layer: extern struct {
+            wl: *ZwlrLayerSurfaceV1
+        },
+        subsurface: extern struct {
+            wl: *WlSubsurface
+        },
+    };
+
+    link: WlList = .{},
+    dirtlink: WlList = .{},
+    state: *State = undefined,
     wl: extern struct {
         surface: *WlSurface,
         xdg_surface: ?*XdgSurface,
         viewport: ?*WpViewport,
         frame_cb: ?*WlCallback,
-    },
-    renderer: Renderer,
-    width: u32,
-    height: u32,
-    desired_width: u32,
-    desired_height: u32,
-    current_width: u32,
-    current_height: u32,
-    configure_serial: u32,
-    scale: c_int,
-    parent: ?*Surface,
-    subsurfaces: WlList,
-    outputs: SurfaceOutputs,
-    flags: SurfaceFlags,
-    states: SurfaceState,
-    title: ?[*:0]u8,
-    role_id: SurfaceRoleId,
-    role: SurfaceRoleUnion,
-    frame: u32,
-    impl: SurfaceImpl,
-    userdata: ?*anyopaque,
+    } = undefined,
+    renderer: Renderer = .{},
+    width: u32 = undefined,
+    height: u32 = undefined,
+    desired_width: u32 = 640,
+    desired_height: u32 = 480,
+    current_width: u32 = undefined,
+    current_height: u32 = undefined,
+    configure_serial: u32 = undefined,
+    scale: c_int = undefined,
+    parent: ?*Surface = undefined,
+    subsurfaces: WlList = .{},
+    outputs: extern struct {
+        outputs: [*]*Output,
+        amount: u32,
+    } = undefined,
+    flags: Flags = .{},
+    states: SurfaceStates = undefined,
+    title: ?[*:0]u8 = null,
+    role_id: RoleId = undefined,
+    role: RoleUnion = undefined,
+    frame: u32 = 0,
+    impl: SurfaceImpl = .{},
+    userdata: ?*anyopaque = undefined,
     extern fn nwl_surface_destroy(surface: *Surface) void;
     extern fn nwl_surface_destroy_later(surface: *Surface) void;
     extern fn nwl_surface_set_vp_destination(surface: *Surface, width: i32, height: i32) bool;
@@ -475,9 +456,6 @@ pub const Surface = extern struct {
             wl_proxy_marshal(@ptrCast(*WlProxy, self.wl.surface), @as(u32, 6));
         }
     }
-    pub fn setSize(self: *Surface, width: u32, height: u32) void {
-        nwl_surface_set_size(self, width, height);
-    }
     pub fn roleTopLevel(self: *Surface) Error!void {
         if (!nwl_surface_role_toplevel(self)) {
             return Error.RoleSetFailed;
@@ -498,9 +476,8 @@ pub const Surface = extern struct {
             return Error.RoleSetFailed;
         }
     }
-    pub fn render(self: *Surface) void {
-        nwl_surface_render(self);
-    }
+    pub const setSize = nwl_surface_set_size;
+    pub const render = nwl_surface_render;
     pub const setTitle = nwl_surface_set_title;
     pub const setVpDestination = nwl_surface_set_vp_destination;
     pub fn swapBuffers(self: *Surface, x: i32, y: i32) void {
@@ -527,6 +504,7 @@ pub const Surface = extern struct {
 };
 
 pub const State = extern struct {
+    pub const PollCallbackFn = *const fn (*State, ?*anyopaque) callconv(.C) void;
     wl: extern struct {
         display: ?*WlDisplay = null,
         registry: ?*WlRegistry = null,
@@ -551,7 +529,12 @@ pub const State = extern struct {
     num_surfaces: u32 = 0,
     run_with_zero_surfaces: bool = false,
     poll: ?*Poll = null,
-    events: StateEventsImpl = .{},
+    events: extern struct {
+        output_new: ?*const fn (*Output) callconv(.C) void = null,
+        output_destroy: ?*const fn (*Output) callconv(.C) void = null,
+        global_add: ?*const fn (*State, *WlRegistry, u32, [*:0]const u8, u32) callconv(.C) bool = null,
+        global_remove: ?*const fn (*State, *WlRegistry, u32) callconv(.C) void = null,
+    } = .{},
     xdg_app_id: ?[*:0]const u8 = null,
     extern fn nwl_wayland_init(state: *State) u8;
     extern fn nwl_wayland_uninit(state: *State) void;
@@ -559,7 +542,7 @@ pub const State = extern struct {
     extern fn nwl_surface_create(state: *State, title: [*:0]const u8) ?*Surface;
     extern fn nwl_state_add_sub(state: *State, subimpl: *StateSubImpl, data: ?*anyopaque) void;
     extern fn nwl_state_get_sub(state: *State, subimpl: *StateSubImpl) ?*anyopaque;
-    extern fn nwl_poll_add_fd(state: *State, fd: c_int, callback: nwl_poll_callback_t, data: ?*anyopaque) void;
+    extern fn nwl_poll_add_fd(state: *State, fd: c_int, callback: PollCallbackFn, data: ?*anyopaque) void;
     extern fn nwl_poll_del_fd(state: *State, fd: c_int) void;
     extern fn nwl_poll_get_fd(state: *State) std.os.fd_t;
     extern fn nwl_poll_dispatch(state: *State, timeout: c_int) bool;
@@ -582,7 +565,6 @@ pub const State = extern struct {
     }
 };
 
-
 pub const ShmPool = extern struct {
     fd:std.os.fd_t = -1,
     data:[*]u8 = undefined,
@@ -590,37 +572,36 @@ pub const ShmPool = extern struct {
     size:usize = 0,
 };
 
-pub const ShmBuffer = extern struct {
-    pub const BufferFlags = packed struct(u8) {
-        acquired:bool = false,
-        destroy:bool = false,
-        _pad:u6 = 0
-    };
-    wl_buffer:?*WlBuffer = null,
-    bufferdata:[*]u8 = undefined,
-    data:?*anyopaque = null,
-    flags:BufferFlags = .{},
-
-    pub fn release(self:*ShmBuffer) void {
-        self.flags.acquired = false;
-    }
-};
-
 pub const ShmBufferMan = extern struct {
-    pub const BufferRendererImpl = extern struct {
-        buffer_create:*const fn(buffer:*ShmBuffer, bufferman:*ShmBufferMan) callconv(.C) void,
-        buffer_destroy:*const fn(buffer:*ShmBuffer, bufferman:*ShmBufferMan) callconv(.C) void,
+    pub const Buffer = extern struct {
+        const Flags = packed struct(u8) {
+            acquired:bool = false,
+            destroy:bool = false,
+            _pad:u6 = 0
+        };
+        wl_buffer:?*WlBuffer = null,
+        bufferdata:[*]u8 = undefined,
+        data:?*anyopaque = null,
+        flags:Flags = .{},
+
+        pub fn release(self:*Buffer) void {
+            self.flags.acquired = false;
+        }
+    };
+    pub const RendererImpl = extern struct {
+        buffer_create:*const fn(buffer:*Buffer, bufferman:*ShmBufferMan) callconv(.C) void,
+        buffer_destroy:*const fn(buffer:*Buffer, bufferman:*ShmBufferMan) callconv(.C) void,
     };
     pool:ShmPool = .{},
-    buffers:[4]ShmBuffer = [_]ShmBuffer{.{}} ** 4,
-    impl:?*const BufferRendererImpl = null,
+    buffers:[4]Buffer = [_]Buffer{.{}} ** 4,
+    impl:?*const RendererImpl = null,
     width:u32 = 0,
     height:u32 = 0,
     stride:u32 = 0,
     format:u32 = 0,
     num_slots:u8 = 1,
 
-    extern fn nwl_shm_bufferman_get_next(bufferman:*ShmBufferMan) ?*ShmBuffer;
+    extern fn nwl_shm_bufferman_get_next(bufferman:*ShmBufferMan) ?*Buffer;
     pub const getNext = nwl_shm_bufferman_get_next;
     extern fn nwl_shm_bufferman_set_slots(bufferman:*ShmBufferMan, state:*State, num_slots:u8) void;
     pub const setSlots = nwl_shm_bufferman_set_slots;
